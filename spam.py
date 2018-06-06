786e9422fe3776f4993a21a3bbf18a26522739a3
Szymek <szymon.rzadca@gmail.com>
2018-06-06 11:51:04 +0200

    more scope exercises

--- spam.py
+++ spam.py
@@ -3,15 +3,19 @@ def spam1():
     def spam2():
 
         def spam3():
-            z = " even more spam"
+            z = " even"  # z must exist before spam 2 is called
+            z += y  # do not combine these assignments
+            print("In spam3, locals are {}".format(locals()))
             return z
 
-        y = " more spam"
-        y += spam3()
+        y = " more " + x  # y must exist before spam 2 is called
+        y += spam3()  # do not combine these assignments
+        print("In spam2, locals are {}".format(locals()))
         return y
 
-    x = "spam"
-    x += spam2()
+    x = "spam"  # x must exist before spam 2 is called
+    x += spam2()  # do not combine these assignments
+    print("In spam3, locals are {}".format(locals()))
     return x
 
 
